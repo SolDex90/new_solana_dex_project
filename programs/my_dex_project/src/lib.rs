839d628a4c5bb4989de1d225bbf93baf205af54e
SolDex90 <solanadex1@gmail.com>
2024-05-28 22:00:11 -0400

    Added token swap functionality and other updates

--- programs/my_dex_project/src/lib.rs
+++ programs/my_dex_project/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self as token_module, Token, TokenAccount as SplTokenAccount};
 use solana_program::entrypoint::ProgramResult;
+use rand::seq::SliceRandom;
 
 declare_id!("2MTDZGGZ7kU8tnscXjZ8LTAiE1F8hmxmhiNEnff6i3kh");
 
@@ -27,16 +28,63 @@ pub mod my_dex_project {
     }
 
     pub fn transfer(ctx: Context<Transfer>, amount: u64) -> ProgramResult {
-        let cpi_accounts = Box::new(token_module::Transfer {
+        let cpi_accounts = token_module::Transfer {
             from: ctx.accounts.from.to_account_info(),
             to: ctx.accounts.to.to_account_info(),
             authority: ctx.accounts.authority.to_account_info(),
-        });
+        };
         let cpi_program = ctx.accounts.token_program.to_account_info();
-        let cpi_ctx = CpiContext::new(cpi_program, *cpi_accounts);
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token_module::transfer(cpi_ctx, amount)?;
         Ok(())
     }
+
+    pub fn place_order(ctx: Context<PlaceOrder>, order: Order) -> ProgramResult {
+        let market = &mut ctx.accounts.market;
+        market.orders.push(order);
+        Ok(())
+    }
+
+    pub fn match_orders(ctx: Context<MatchOrders>) -> ProgramResult {
+        let market = &mut ctx.accounts.market;
+
+        let mut rng = rand::thread_rng();
+        market.orders.shuffle(&mut rng);
+
+        for i in 0..market.orders.len() {
+            for j in (i + 1)..market.orders.len() {
+                let order_a = &market.orders[i];
+                let order_b = &market.orders[j];
+
+                if order_a.order_type != order_b.order_type && order_a.price == order_b.price {
+                    // Match orders
+                    market.orders.remove(j);
+                    market.orders.remove(i);
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq)]
+pub enum OrderType {
+    Buy,
+    Sell,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Order {
+    pub order_type: OrderType,
+    pub amount: u64,
+    pub price: u64,
+}
+
+#[account]
+pub struct Market {
+    pub orders: Vec<Order>,
 }
 
 #[derive(Accounts)]
@@ -61,9 +109,8 @@ pub struct MyAccount {
 
 #[derive(Accounts)]
 pub struct InitializeToken<'info> {
-    /// CHECK: This account is allowed to be unchecked as it's a signer.
-    #[account(mut, signer)]
-    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
     #[account(init, payer = authority, space = 8 + 8)]
     pub token_account: Account<'info, CustomTokenAccount>,
     pub system_program: Program<'info, System>,
@@ -75,13 +122,27 @@ pub struct Transfer<'info> {
     pub from: Account<'info, SplTokenAccount>,
     #[account(mut)]
     pub to: Account<'info, SplTokenAccount>,
-    /// CHECK: This account is allowed to be unchecked as it's a signer.
-    #[account(mut, signer)]
-    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
     pub token_program: Program<'info, Token>,
 }
 
 #[account]
 pub struct CustomTokenAccount {
     pub amount: u64,
+}
+
+#[derive(Accounts)]
+pub struct PlaceOrder<'info> {
+    #[account(mut)]
+    pub market: Account<'info, Market>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MatchOrders<'info> {
+    #[account(mut)]
+    pub market: Account<'info, Market>,
 }

d7ddf2d537ce6c47dbad25019534e5bce10168dc
SolDex90 <solanadex1@gmail.com>
2024-05-31 17:40:25 -0400

    Initial setup and basic UI using Material-UI

--- solana-dex-frontend/config-overrides.js
+++ solana-dex-frontend/config-overrides.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
+  // Add fallback for node core modules
   config.resolve.fallback = {
     ...config.resolve.fallback,
     http: require.resolve('stream-http'),
@@ -9,12 +10,19 @@ module.exports = function override(config) {
     url: require.resolve('url/'),
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
+    vm: require.resolve('vm-browserify'),  // Add fallback for 'vm'
   };
+
+  // Provide global variables
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
     }),
   ]);
+
+  // Ignore source map warnings
+  config.ignoreWarnings = [/Failed to parse source map/];
+
   return config;
 };

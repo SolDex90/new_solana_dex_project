691a83301c223add7a628d1bf02837799dcd3ff5
SolDex90 <solanadex1@gmail.com>
2024-06-02 16:43:10 -0400

    Clean install dependencies and remove old build files

--- solana-dex-frontend/build/static/js/221.d66f5f56.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksolana_dex_frontend=self.webpackChunksolana_dex_frontend||[]).push([[221],{2221:(e,n,a)=>{a.r(n),a.d(n,{default:()=>s});a(5043);var d=a(579);const s=()=>(0,d.jsx)("div",{children:(0,d.jsx)("h2",{children:"Social Trading"})})}}]);
-//# sourceMappingURL=221.d66f5f56.chunk.js.map

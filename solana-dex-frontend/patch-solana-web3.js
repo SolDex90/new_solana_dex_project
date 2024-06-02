28b49ec3ca35416ea6c21f6cf12365b22ef2dbe8
SolDex90 <solanadex1@gmail.com>
2024-06-02 18:41:08 -0400

    still not going to work

--- solana-dex-frontend/patch-solana-web3.js
+++ solana-dex-frontend/patch-solana-web3.js
@@ -2,20 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.resolve(__dirname, 'node_modules/@solana/web3.js/lib/index.js');
+const fileContent = fs.readFileSync(filePath, 'utf8');
+const fixedContent = fileContent.replace(
+  "require('rpc-websockets/dist/lib/client/websocket.browser')",
+  "require('rpc-websockets')"
+);
 
-console.log(`Checking for file: ${filePath}`);
-
-if (fs.existsSync(filePath)) {
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-
-  const patchedContent = fileContent.replace(
-    "import { WebSocket } from 'rpc-websockets/dist/lib/client/websocket.browser';",
-    "import { WebSocket } from 'rpc-websockets';"
-  );
-
-  fs.writeFileSync(filePath, patchedContent, 'utf8');
-  console.log('Patch applied successfully.');
-} else {
-  console.error(`File not found: ${filePath}`);
-  process.exit(1); // Exit with an error code
-}
+fs.writeFileSync(filePath, fixedContent, 'utf8');

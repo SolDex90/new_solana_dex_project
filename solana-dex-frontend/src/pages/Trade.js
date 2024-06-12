51a599a6962cf997a649cb5138595aa1507b168e
SolDex90 <solanadex1@gmail.com>
2024-06-11 22:26:18 -0400

    redirect homepage

--- solana-dex-frontend/src/pages/Trade.js
+++ solana-dex-frontend/src/pages/Trade.js
@@ -1,13 +1,11 @@
-// src/pages/Trade.js
-
 import React from 'react';
-import Swap from '../components/Swap'; // Adjust the import path if necessary
+import TokenSwap from '../components/TokenSwap'; // Adjust the import path if necessary
 import '../styles/styles.css'; // Ensure the path is correct
 
 const Trade = () => {
   return (
     <main className="trade-page">
-      <Swap />
+      <TokenSwap />
     </main>
   );
 };

fa4b7e1fbbf81f93b210c799ae114eb149cecbe0
SolDex90 <solanadex1@gmail.com>
2024-06-13 21:55:40 -0400

    added pool for token sniping

--- solana-dex-frontend/src/pages/TokenSniper.js
+++ solana-dex-frontend/src/pages/TokenSniper.js
@@ -4,7 +4,7 @@ import '../styles/sniping.css'; // Ensure the path is correct
 
 const TokenSniper = () => {
   return (
-    <div>
+    <div className="token-sniper-page">
       <h2>Token Sniper</h2>
       <p>This is the Token Sniper page. Here you can snipe new tokens released on BirdEye.</p>
       <TokenSniperBot /> {/* Integrate the TokenSniperBot component */}
